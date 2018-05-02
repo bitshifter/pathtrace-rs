9767525efddaf61fdc435c3d78524b84f5b60cfd
Cameron Hart <cameron.hart@gmail.com>
2018-05-03 07:38:50 +1000

    Write directly to output buffer.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use rayon::prelude::*;
 use scene::Scene;
 use std::f32;
 use std::time::SystemTime;
-use vmath::{Vec3, vec3};
+use vmath::vec3;
 
 fn main() {
     let matches = App::new("Toy Path Tracer")
@@ -42,11 +42,16 @@ fn main() {
     let nx = value_t!(matches, "width", u32).unwrap_or(1200);
     let ny = value_t!(matches, "height", u32).unwrap_or(800);
     let ns = value_t!(matches, "samples", u32).unwrap_or(10);
+    let bpp = 3;
 
     println!(
         "generating {}x{} image with {} samples per pixel",
         nx, ny, ns
     );
+
+    let inv_nx = 1.0 / nx as f32;
+    let inv_ny = 1.0 / ny as f32;
+    let inv_ns = 1.0 / ns as f32;
 
     // Not writing crypto so use a weak rng, also pass it around to avoid construction cost.
     let scene = Scene::random_scene(&mut rand::weak_rng());
@@ -65,50 +70,45 @@ fn main() {
         dist_to_focus,
     );
 
-    let mut pixels: Vec<Vec3> = std::iter::repeat(vec3(0.0, 0.0, 0.0))
-        .take((nx * ny) as usize)
+    let mut buffer: Vec<u8> = std::iter::repeat(0)
+        .take((nx * ny * bpp) as usize)
         .collect();
 
     let start_time = SystemTime::now();
 
-    let inv_nx = 1.0 / nx as f32;
-    let inv_ny = 1.0 / ny as f32;
-
     // parallel iterate each row of pixels
-    pixels
-        .par_chunks_mut(nx as usize)
+    buffer
+        .par_chunks_mut((nx * bpp) as usize)
         .rev()
         .enumerate()
         .for_each(|(j, row)| {
-            for (i, rgb) in row.iter_mut().enumerate() {
+            for (i, rgb) in row.chunks_mut(bpp as usize).enumerate() {
                 let mut rng = rand::weak_rng();
+                let mut col = vec3(0.0, 0.0, 0.0);
                 for _ in 0..ns {
                     let u = (i as f32 + rng.next_f32()) * inv_nx;
                     let v = (j as f32 + rng.next_f32()) * inv_ny;
                     let ray = camera.get_ray(u, v, &mut rng);
-                    *rgb += scene.ray_trace(&ray, 0, &mut rng);
+                    col += scene.ray_trace(&ray, 0, &mut rng);
                 }
-                *rgb /= ns as f32;
+                col *= inv_ns;
+                let mut iter = rgb.iter_mut();
+                *iter.next().unwrap() = (255.99 * col.x.sqrt()) as u8;
+                *iter.next().unwrap() = (255.99 * col.y.sqrt()) as u8;
+                *iter.next().unwrap() = (255.99 * col.z.sqrt()) as u8;
             }
         });
 
     let elapsed = start_time
         .elapsed()
         .expect("SystemTime elapsed time failed");
+
     println!(
         "{}.{:.2} seconds {} rays",
         elapsed.as_secs(),
         elapsed.subsec_nanos() * 1_000_000_000,
         scene.ray_count()
     );
-
-    // write colour values out as bytes and save as image
-    let mut buffer = Vec::with_capacity((nx * ny * 3) as usize);
-    for col in pixels {
-        buffer.push((255.99 * col.x.sqrt()) as u8);
-        buffer.push((255.99 * col.y.sqrt()) as u8);
-        buffer.push((255.99 * col.z.sqrt()) as u8);
-    }
 
     image::save_buffer("output.png", &buffer, nx, ny, image::RGB(8))
         .expect("Failed to save output image");
