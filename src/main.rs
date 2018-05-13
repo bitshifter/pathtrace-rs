c150e4b189079cfd32c544df8de7abc11fabb803
Cameron Hart <cameron.hart@gmail.com>
2018-05-14 07:36:34 +1000

    Render in-progress image with glium.
    Inspired by @aras_p's path tracer.
    
    Saves the framebuffer to disk on exit.
    
    There's an issue where closing the window will cause a crash and
    hitting escape to exit is not very responsive mid update.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate clap;
+#[macro_use]
+extern crate glium;
 extern crate image;
 extern crate rand;
 extern crate rayon;
@@ -11,13 +13,10 @@ mod scene;
 mod vmath;
 
 use clap::{App, Arg};
-use rand::{Rng, SeedableRng, XorShiftRng};
-use rayon::prelude::*;
-use std::f32;
+use glium::{Surface, glutin::{Api, GlProfile, GlRequest}, index::{NoIndices, PrimitiveType},
+            texture::buffer_texture::{BufferTexture, BufferTextureType},
+            vertex::EmptyVertexAttributes};
 use std::time::SystemTime;
-use vmath::vec3;
-
-const FIXED_SEED: [u32; 4] = [0x193a_6754, 0xa8a7_d469, 0x9783_0e05, 0x113b_a7bb];
 
 fn main() {
     let matches = App::new("Toy Path Tracer")
@@ -55,83 +54,145 @@ fn main() {
         ])
         .get_matches();
 
-    let nx = value_t!(matches, "width", u32).unwrap_or(1280);
-    let ny = value_t!(matches, "height", u32).unwrap_or(720);
-    let ns = value_t!(matches, "samples", u32).unwrap_or(4);
-    let max_depth = value_t!(matches, "depth", u32).unwrap_or(50);
-    let preset = matches.value_of("preset").unwrap_or("random");
-    let channels = 3;
+    let params = scene::Params {
+        width: value_t!(matches, "width", u32).unwrap_or(1280),
+        height: value_t!(matches, "height", u32).unwrap_or(720),
+        samples: value_t!(matches, "samples", u32).unwrap_or(4),
+        max_depth: value_t!(matches, "depth", u32).unwrap_or(50),
+        random_seed: matches.is_present("random"),
+    };
+
+    let preset = matches.value_of("preset").unwrap_or("aras");
+
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new()
+        .with_dimensions(params.width, params.height)
+        .with_title("pathtrace-rs");
+    let context = glium::glutin::ContextBuilder::new()
+        .with_vsync(true)
+        .with_gl(GlRequest::Specific(Api::OpenGl, (3, 2)))
+        .with_gl_profile(GlProfile::Core);
+    let display =
+        glium::Display::new(window, context, &events_loop).expect("Failed to create display");
+
+    let mut buffer_texture: BufferTexture<(u8, u8, u8, u8)> =
+        BufferTexture::empty_persistent(
+            &display,
+            (params.width * params.height * 4) as usize,
+            BufferTextureType::Float,
+        ).expect("Failed to create rgb_buffer texture");
+
+    let program = glium::Program::from_source(
+        &display,
+        "
+            #version 330 core
+
+            void main() {
+                const vec4 vertices[] = vec4[](vec4(-1.0, -1.0, 0.5, 1.0),
+                                               vec4( 1.0, -1.0, 0.5, 1.0),
+                                               vec4(-1.0,  1.0, 0.5, 1.0),
+                                               vec4( 1.0,  1.0, 0.5, 1.0));
+
+                gl_Position = vertices[gl_VertexID];
+            }
+        ",
+        "
+            #version 330 core
+
+            uniform int stride;
+            uniform samplerBuffer tex;
+            out vec4 color;
+
+            void main() {
+                int x = int(gl_FragCoord.x);
+                int y = int(gl_FragCoord.y);
+                int index = y * stride + x;
+                color = texelFetch(tex, index);
+            }
+        ",
+        None,
+    ).expect("Failed to create shader");
 
     println!(
         "generating '{}' preset at {}x{} with {} samples per pixel",
-        preset, nx, ny, ns
+        preset, params.width, params.height, params.samples
     );
 
-    let random_seed = matches.is_present("random");
-    let weak_rng = || {
-        if random_seed {
-            rand::weak_rng()
-        } else {
-            XorShiftRng::from_seed(FIXED_SEED)
-        }
-    };
+    let (scene, camera) = presets::from_name(preset, &params).expect("unrecognised preset");
 
-    let (scene, camera) = presets::from_name(
-        preset,
-        &presets::Params {
-            width: nx,
-            height: ny,
-            max_depth,
-        },
-        &mut weak_rng(),
-    ).expect("unrecognised preset");
+    let mut rgb_buffer = vec![(0.0, 0.0, 0.0); (params.width * params.height) as usize];
 
-    let inv_nx = 1.0 / nx as f32;
-    let inv_ny = 1.0 / ny as f32;
-    let inv_ns = 1.0 / ns as f32;
-
-    let mut buffer = vec![0u8; (nx * ny * channels) as usize];
-
-    let start_time = SystemTime::now();
-
-    // parallel iterate each row of pixels
-    buffer
-        .par_chunks_mut((nx * channels) as usize)
-        .rev()
-        .enumerate()
-        .for_each(|(j, row)| {
-            row.chunks_mut(channels as usize)
-                .enumerate()
-                .for_each(|(i, rgb)| {
-                    let mut rng = weak_rng();
-                    let mut col = vec3(0.0, 0.0, 0.0);
-                    for _ in 0..ns {
-                        let u = (i as f32 + rng.next_f32()) * inv_nx;
-                        let v = (j as f32 + rng.next_f32()) * inv_ny;
-                        let ray = camera.get_ray(u, v, &mut rng);
-                        col += scene.ray_trace(&ray, 0, &mut rng);
-                    }
-                    col *= inv_ns;
-                    let mut iter = rgb.iter_mut();
-                    *iter.next().unwrap() = (255.99 * col.x.sqrt()) as u8;
-                    *iter.next().unwrap() = (255.99 * col.y.sqrt()) as u8;
-                    *iter.next().unwrap() = (255.99 * col.z.sqrt()) as u8;
-                });
+    let mut frame_num = 0;
+    loop {
+        let mut quit = false;
+        events_loop.poll_events(|event| {
+            use glium::glutin::{ElementState, Event, VirtualKeyCode, WindowEvent};
+            if let Event::WindowEvent { event, .. } = event {
+                match event {
+                    WindowEvent::Closed => quit = true,
+                    WindowEvent::KeyboardInput { input, .. } => {
+                        if let ElementState::Released = input.state {
+                            if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
+                                quit = true;
+                            }
+                        }
+                    },
+                    _ => (),
+                };
+            }
         });
+        if quit {
+            break;
+        }
 
-    let elapsed = start_time
-        .elapsed()
-        .expect("SystemTime elapsed time failed");
-    let elapsed_secs = elapsed.as_secs() as f64 + (elapsed.subsec_nanos() as f64) / 1000_000_000.0;
-    let ray_count = scene.ray_count();
+        let start_time = SystemTime::now();
+        let ray_count = scene.update(&params, &camera, frame_num, &mut rgb_buffer);
+        frame_num += 1;
 
-    println!(
-        "{:.2}secs {}rays {:.2}Mrays/s",
-        elapsed_secs,
-        ray_count,
-        ray_count as f64 / 1_000_000.0 / elapsed_secs
-    );
+        let elapsed = start_time
+            .elapsed()
+            .expect("SystemTime elapsed time failed");
+        let elapsed_secs =
+            elapsed.as_secs() as f64 + (elapsed.subsec_nanos() as f64) / 1_000_000_000.0;
+        let ray_count = ray_count as f64 / 1_000_000.0;
 
-    image::save_buffer("output.png", &buffer, nx, ny, image::RGB(8))
+        println!(
+            "{:.2}secs {:.2}Mrays/s {:.2}Mrays/frame {}frames",
+            elapsed_secs,
+            ray_count / elapsed_secs,
+            ray_count,
+            frame_num
+        );
+
+        {
+            let mut mapping = buffer_texture.map();
+            for (color_out, color_in) in mapping.iter_mut().zip(rgb_buffer.iter()) {
+                color_out.0 = (255.99 * color_in.0) as u8;
+                color_out.1 = (255.99 * color_in.1) as u8;
+                color_out.2 = (255.99 * color_in.2) as u8;
+                color_out.3 = 255;
+            }
+        }
+
+        let mut target = display.draw();
+        target
+            .draw(
+                EmptyVertexAttributes { len: 4 },
+                NoIndices(PrimitiveType::TriangleStrip),
+                &program,
+                &uniform!{ tex: &buffer_texture, stride: params.width as i32 },
+                &Default::default(),
+            )
+            .unwrap();
+        target.finish().unwrap();
+    }
+
+    // reading the front rgb_buffer into an image
+    let image: glium::texture::RawImage2d<u8> = display.read_front_buffer();
+    let image =
+        image::ImageBuffer::from_raw(image.width, image.height, image.data.into_owned()).unwrap();
+    let image = image::DynamicImage::ImageRgba8(image).flipv().to_rgb();
+    image
+        .save("output.png")
         .expect("Failed to save output image");
 }
--- src/scene.rs
+++ src/scene.rs
@@ -1,5 +1,7 @@
+use camera::Camera;
 use math::{random_in_unit_sphere, random_unit_vector, ray, Ray};
-use rand::{Rng, XorShiftRng};
+use rand::{weak_rng, Rng, SeedableRng, XorShiftRng};
+use rayon::prelude::*;
 use std::f32;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
@@ -148,20 +150,26 @@ impl Sphere {
     }
 }
 
+pub struct Params {
+    pub width: u32,
+    pub height: u32,
+    pub samples: u32,
+    pub max_depth: u32,
+    pub random_seed: bool,
+}
+
 pub struct Scene {
     spheres: Vec<Sphere>,
     materials: Vec<Material>,
-    max_depth: u32,
     ray_count: AtomicUsize,
 }
 
 impl Scene {
-    pub fn new(sphere_materials: &[(Sphere, Material)], max_depth: u32) -> Scene {
+    pub fn new(sphere_materials: &[(Sphere, Material)]) -> Scene {
         let (spheres, materials) = sphere_materials.iter().cloned().unzip();
         Scene {
             spheres,
             materials,
-            max_depth,
             ray_count: AtomicUsize::new(0),
         }
     }
@@ -178,14 +186,14 @@ impl Scene {
         result
     }
 
-    pub fn ray_trace(&self, ray_in: &Ray, depth: u32, rng: &mut XorShiftRng) -> Vec3 {
+    fn ray_trace(&self, ray_in: &Ray, depth: u32, max_depth: u32, rng: &mut XorShiftRng) -> Vec3 {
         const MAX_T: f32 = f32::MAX;
         const MIN_T: f32 = 0.001;
         self.ray_count.fetch_add(1, Ordering::SeqCst);
         if let Some((ray_hit, material)) = self.ray_hit(ray_in, MIN_T, MAX_T) {
-            if depth < self.max_depth {
+            if depth < max_depth {
                 if let Some((attenuation, scattered)) = material.scatter(ray_in, &ray_hit, rng) {
-                    return attenuation * self.ray_trace(&scattered, depth + 1, rng);
+                    return attenuation * self.ray_trace(&scattered, depth + 1, max_depth, rng);
                 }
             }
             return Vec3::zero();
@@ -196,7 +204,47 @@ impl Scene {
         }
     }
 
-    pub fn ray_count(&self) -> usize {
+    pub fn update(
+        &self,
+        params: &Params,
+        camera: &Camera,
+        frame_num: u32,
+        buffer: &mut [(f32, f32, f32)],
+    ) -> usize {
+        self.ray_count.store(0, Ordering::Relaxed);
+
+        let inv_nx = 1.0 / params.width as f32;
+        let inv_ny = 1.0 / params.height as f32;
+        let inv_ns = 1.0 / params.samples as f32;
+
+        let mix_prev = frame_num as f32 / (frame_num + 1) as f32;
+        let mix_new = 1.0 - mix_prev;
+
+        // parallel iterate each row of pixels
+        buffer
+            .par_chunks_mut(params.width as usize)
+            .enumerate()
+            .for_each(|(j, row)| {
+                let mut rng = if params.random_seed {
+                    weak_rng()
+                } else {
+                    let state = (j as u32 * 9781 + frame_num * 6271) | 1;
+                    XorShiftRng::from_seed([state, state, state, state])
+                };
+                row.iter_mut().enumerate().for_each(|(i, color_out)| {
+                    let mut col = vec3(0.0, 0.0, 0.0);
+                    for _ in 0..params.samples {
+                        let u = (i as f32 + rng.next_f32()) * inv_nx;
+                        let v = (j as f32 + rng.next_f32()) * inv_ny;
+                        let ray = camera.get_ray(u, v, &mut rng);
+                        col += self.ray_trace(&ray, 0, params.max_depth, &mut rng);
+                    }
+                    col *= inv_ns;
+                    color_out.0 = color_out.0 * mix_prev + col.x * mix_new;
+                    color_out.1 = color_out.1 * mix_prev + col.y * mix_new;
+                    color_out.2 = color_out.2 * mix_prev + col.z * mix_new;
+                });
+            });
         self.ray_count.load(Ordering::Relaxed)
     }
 }
