1b0ad6a6bf46f8626fbd576afbf3dc16db81528c
Cameron Hart <cameron.hart@gmail.com>
2018-11-01 13:30:02 +1100

    Updated glium

--- src/glium_window.rs
+++ src/glium_window.rs
@@ -13,7 +13,7 @@ use std::time::{Duration, SystemTime};
 pub fn start_loop(params: Params, camera: Camera, scene: Scene, max_frames: Option<u32>) {
     let mut events_loop = glium::glutin::EventsLoop::new();
     let window = glium::glutin::WindowBuilder::new()
-        .with_dimensions(params.width, params.height)
+        .with_dimensions((params.width, params.height).into())
         .with_title("pathtrace-rs");
     let context = glium::glutin::ContextBuilder::new()
         .with_vsync(true)
@@ -117,22 +117,16 @@ pub fn start_loop(params: Params, camera: Camera, scene: Scene, max_frames: Opti
 
     let mut frame_num = 0;
     let mut quit = false;
-    let mut save = false;
     while !quit {
         events_loop.poll_events(|event| {
             use glium::glutin::{ElementState, Event, VirtualKeyCode, WindowEvent};
             if let Event::WindowEvent { event, .. } = event {
                 match event {
-                    // TODO: glutin 0.15 support this which is needed so we can save on exit
-                    // Until then we only get a message when the window is already closed and the
-                    // GL context is dead, so we can't save the frame buffer.
-                    // WindowEvent::CloseRequested => quit = true,
-                    WindowEvent::Closed => quit = true,
+                    WindowEvent::CloseRequested => quit = true,
                     WindowEvent::KeyboardInput { input, .. } => {
                         if let ElementState::Released = input.state {
                             if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
                                 quit = true;
-                                save = true;
                             }
                         }
                     }
@@ -194,16 +188,14 @@ pub fn start_loop(params: Params, camera: Camera, scene: Scene, max_frames: Opti
         };
     }
 
-    if save {
-        // reading the front rgb_buffer into an image
-        let image: glium::texture::RawImage2d<u8> = display.read_front_buffer();
-        let image =
-            image::ImageBuffer::from_raw(image.width, image.height, image.data.into_owned()).unwrap();
-        let image = image::DynamicImage::ImageRgba8(image).flipv().to_rgb();
-        image
-            .save("output.png")
-            .expect("Failed to save output image");
-    }
+    // reading the front rgb_buffer into an image
+    let image: glium::texture::RawImage2d<u8> = display.read_front_buffer();
+    let image =
+        image::ImageBuffer::from_raw(image.width, image.height, image.data.into_owned()).unwrap();
+    let image = image::DynamicImage::ImageRgba8(image).flipv().to_rgb();
+    image
+        .save("output.png")
+        .expect("Failed to save output image");
 
     // tell the worker to exit
     main_send.send(None).unwrap();
