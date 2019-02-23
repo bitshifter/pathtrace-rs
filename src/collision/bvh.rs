251e1226d419fce403ce4760d69cee70828bf69c
Cameron Hart <cameron.hart@gmail.com>
2019-02-23 11:36:54 +1100

    Special case 0, 1 and 2 sized bvh roots.

--- src/collision/bvh.rs
+++ src/collision/bvh.rs
@@ -72,30 +72,8 @@ impl<'a> BVHNode<'a> {
         hitables: &mut [Hitable<'a>],
         arena: &'a Arena<BVHNode<'a>>,
     ) -> Option<&'a BVHNode<'a>> {
-        let root = BVHNode::new_split(rng, hitables, arena, 0.0, 0.0);
-        // dbg!(&root);
-        root
-    }
-
-    pub fn new_split(
-        rng: &mut XorShiftRng,
-        hitables: &mut [Hitable<'a>],
-        arena: &'a Arena<BVHNode<'a>>,
-        t0: f32,
-        t1: f32,
-    ) -> Option<&'a BVHNode<'a>> {
-        let axis = rng.next_u32() % 3;
-        hitables.sort_unstable_by(|lhs, rhs| {
-            let lhs_min = lhs.bounding_box(t0, t1).unwrap().min;
-            let rhs_min = rhs.bounding_box(t0, t1).unwrap().min;
-            let ord = match axis {
-                0 => lhs_min.get_x().partial_cmp(&rhs_min.get_x()),
-                1 => lhs_min.get_y().partial_cmp(&rhs_min.get_y()),
-                2 => lhs_min.get_z().partial_cmp(&rhs_min.get_z()),
-                _ => unreachable!(),
-            };
-            ord.unwrap()
-        });
+        let t0 = 0.0;
+        let t1 = 0.0;
         match hitables.len() {
             0 => None,
             1 => {
@@ -113,14 +91,70 @@ impl<'a> BVHNode<'a> {
                 Some(alloc_from_hittables(arena, lhs, rhs, aabb))
             }
             _ => {
+                Some(BVHNode::new_split(rng, hitables, arena, t0, t1))
+                // Some(BVHNode::new_root(rng, hitables, areana)),
+            }
+        }
+    }
+
+    // fn new_root(
+    //     rng: &mut XorShiftRng,
+    //     hitables: &mut [Hitable<'a>],
+    //     arena: &'a Arena<BVHNode<'a>>,
+    // ) -> &'a BVHNode<'a> {
+    //     BVHNode::new_split(rng, hitables, arena, 0.0, 0.0);
+    // }
+
+    // fn new_branch(
+    //     rng: &mut XorShiftRng,
+    //     hitables: &mut [Hitable<'a>],
+    //     arena: &'a Arena<BVHNode<'a>>,
+    //     ) -> Hitable<'a> {
+    // }
+
+    fn new_split(
+        rng: &mut XorShiftRng,
+        hitables: &mut [Hitable<'a>],
+        arena: &'a Arena<BVHNode<'a>>,
+        t0: f32,
+        t1: f32,
+    ) -> &'a BVHNode<'a> {
+        let axis = rng.next_u32() % 3;
+        hitables.sort_unstable_by(|lhs, rhs| {
+            let lhs_min = lhs.bounding_box(t0, t1).unwrap().min;
+            let rhs_min = rhs.bounding_box(t0, t1).unwrap().min;
+            let ord = match axis {
+                0 => lhs_min.get_x().partial_cmp(&rhs_min.get_x()),
+                1 => lhs_min.get_y().partial_cmp(&rhs_min.get_y()),
+                2 => lhs_min.get_z().partial_cmp(&rhs_min.get_z()),
+                _ => unreachable!(),
+            };
+            ord.unwrap()
+        });
+        match hitables.len() {
+            0 => unreachable!(),
+            1 => {
+                let lhs = hitables[0];
+                let rhs = lhs;
+                let aabb = lhs.bounding_box(t0, t1).unwrap();
+                alloc_from_hittables(arena, lhs, rhs, aabb)
+            }
+            2 => {
+                let lhs = hitables[0];
+                let rhs = hitables[1];
+                let lhs_aabb = lhs.bounding_box(t0, t1).unwrap();
+                let rhs_aabb = rhs.bounding_box(t0, t1).unwrap();
+                let aabb = lhs_aabb.add(&rhs_aabb);
+                alloc_from_hittables(arena, lhs, rhs, aabb)
+            }
+            _ => {
                 let pivot = hitables.len() / 2;
-                let lhs = BVHNode::new_split(rng, &mut hitables[..pivot], arena, t0, t1).unwrap();
-                let rhs =
-                    BVHNode::new_split(rng, &mut hitables[pivot..], arena, t0, t1).unwrap();
+                let lhs = BVHNode::new_split(rng, &mut hitables[..pivot], arena, t0, t1);
+                let rhs = BVHNode::new_split(rng, &mut hitables[pivot..], arena, t0, t1);
                 let lhs_aabb = lhs.aabb;
                 let rhs_aabb = rhs.aabb;
                 let aabb = lhs_aabb.add(&rhs_aabb);
-                Some(alloc_from_nodes(arena, lhs, rhs, aabb))
+                alloc_from_nodes(arena, lhs, rhs, aabb)
             }
         }
     }
